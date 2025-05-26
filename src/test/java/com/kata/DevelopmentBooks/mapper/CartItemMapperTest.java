73922338c038e0cf2af9858f2ec23eb45c50a3e5
Vijayvignesh G S <vijay2340025@gmail.com>
2025-05-26 16:41:09 +0530

    feat: add CartItemMapper implementation

---
+++ src/test/java/com/kata/DevelopmentBooks/mapper/CartItemMapperTest.java
@@ -0,0 +1,47 @@
+package com.kata.DevelopmentBooks.mapper;
+
+import com.kata.DevelopmentBooks.dto.CartItemDto;
+import com.kata.DevelopmentBooks.model.CartItem;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CartItemMapperTest {
+
+    @Autowired
+    private CartItemMapper cartItemMapper;
+
+    @Test
+    @DisplayName("should map CartItem to CartItemDto")
+    void shouldMapCartItemToCartItemDto() {
+        CartItem cartItem = new CartItem(UUID.randomUUID().toString(), "prod001", 2);
+        CartItemDto cartItemDto = cartItemMapper.toCartItemDto(cartItem);
+
+        assertNull(cartItemMapper.toCartItemDto(null));
+
+        assertNotNull(cartItemDto);
+        assertEquals(cartItem.getItemId(), cartItemDto.getItemId());
+        assertEquals(cartItem.getProductId(), cartItemDto.getProductId());
+        assertEquals(cartItem.getQuantity(), cartItemDto.getQuantity());
+    }
+
+    @Test
+    @DisplayName("should map CartItemDto to CartItem")
+    void shouldMapCartItemDtoToCartItem() {
+        CartItemDto cartItemDto = new CartItemDto(UUID.randomUUID().toString(), "prod001", 5);
+        CartItem cartItem = cartItemMapper.toCartItem(cartItemDto);
+
+        assertNull(cartItemMapper.toCartItem(null));
+
+        assertNotNull(cartItemDto);
+        assertEquals(cartItemDto.getItemId(), cartItem.getItemId());
+        assertEquals(cartItemDto.getProductId(), cartItem.getProductId());
+        assertEquals(cartItemDto.getQuantity(), cartItem.getQuantity());
+    }
+}
